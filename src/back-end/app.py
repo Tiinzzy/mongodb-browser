2a27d92516275b8af0393ae7f048685887f0c9b9
Tina <tinzzy.cracked@yahoo.com>
2023-08-18 18:26:52 -0400

    changed API's to test two application via proxy

--- src/back-end/app.py
+++ src/back-end/app.py
@@ -10,57 +10,62 @@ def get_parameters(req):
     return json.loads(req.data.decode('utf8').replace("'", '"'))
 
 
-@app.route('/mongodb/connect', methods=["POST"])
+@app.route('/db/mongodb/connect', methods=["POST"])
 def connect_mongo_db():
     parameters = get_parameters(request)
     result = app_mongodb.connect(parameters)
     return jsonify(result)
 
 
-@app.route('/mongodb/delete_document', methods=["POST"])
+@app.route('/db/mongodb/delete_document', methods=["POST"])
 def delete_document_mongo_db():
     parameters = get_parameters(request)
     result = app_mongodb.delete_document(parameters)
     return jsonify(result)
 
 
-@app.route('/mongodb/databases', methods=["POST"])
+@app.route('/db/mongodb/databases', methods=["POST"])
 def databases_mongo_db():
     parameters = get_parameters(request)
     result = app_mongodb.get_databases(parameters)
     return jsonify(result)
 
 
-@app.route('/mongodb/collections', methods=["POST"])
+@app.route('/db/mongodb/collections', methods=["POST"])
 def collections_mongo_db():
     parameters = get_parameters(request)
     result = app_mongodb.get_collections(parameters)
     return result
 
 
-@app.route('/mongodb/documents', methods=["POST"])
+@app.route('/db/mongodb/documents', methods=["POST"])
 def documents_mongo_db():
     parameters = get_parameters(request)
     result = app_mongodb.get_documents(parameters)
     return jsonify(result)
 
 
-@app.route('/mongodb/insert_documents', methods=["POST"])
+@app.route('/db/mongodb/insert_documents', methods=["POST"])
 def insert_documents_mongo_db():
     parameters = get_parameters(request)
     result = app_mongodb.insert_documents(parameters)
     return jsonify(result)
 
 
-@app.route('/mongodb/drop_collection', methods=["POST"])
+@app.route('/db/mongodb/drop_collection', methods=["POST"])
 def drop_collection_mongo_db():
     parameters = get_parameters(request)
     result = app_mongodb.drop_collection(parameters)
     return jsonify(result)
 
 
-@app.route('/mongodb/update_document', methods=["POST"])
+@app.route('/db/mongodb/update_document', methods=["POST"])
 def update_document_mongo_db():
     parameters = get_parameters(request)
     result = app_mongodb.update_document(parameters)
     return jsonify(result)
+
+
+@app.route('/db/mongodb/test', methods=["GET"])
+def mogodb_test():
+    return jsonify({'result': 'fantastic'})
