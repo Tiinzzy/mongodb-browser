7d6c221b4f009c610fb1eab8b9ab6ffda14b33bc
Tina <tinzzy.cracked@yahoo.com>
2023-08-22 18:22:40 -0400

    tried to fix the shell script and server.js to run front-end forgot to build the server and have to do tomorrow

--- src/front-end/server/server.js
+++ src/front-end/server/server.js
@@ -5,6 +5,9 @@ const app = express();
 
 const PORT = 3000;
 const HOST = "localhost";
+const STATIC_HOME = process.env.STATIC_HOME
+
+console.log('Static home address for express server:', STATIC_HOME)
 
 const backendProxy = createProxyMiddleware({
     target: "http://localhost:8000",
@@ -14,7 +17,7 @@ const backendProxy = createProxyMiddleware({
 
 app.use('/backend', backendProxy);
 
-app.use(express.static('/home/tina/Documents/projects/mongodb-browser/src/front-end/build/'))
+app.use(express.static(STATIC_HOME))
 
 app.listen(PORT, HOST, () => {
     console.log(`Starting Proxy at ${HOST}:${PORT}`);
